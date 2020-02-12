5967a3160d438a8a5963e56a29a116bf096710cf
Siromom <swon962@gmail.com>
2020-02-12 22:23:15 +0900

    [ADD] project05-transitionedn event

--- 05-FlexPanelGallery/script.js
+++ 05-FlexPanelGallery/script.js
@@ -4,4 +4,13 @@ function toggleOpen() {
   this.classList.toggle("open");
 }
 
+function toggleActive(e) {
+  /* Safari transitionend event.propertyName === flex */
+  /* Chrome + FF transitionend event.propertyName === flex-grow */
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
 panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
