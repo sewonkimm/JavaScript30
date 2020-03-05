e662f4e0be47b4b4008f8b563e3fdf0581e6836c
Siromom <swon962@gmail.com>
2020-03-06 03:04:04 +0900

    [ADD] project8 - drawing function
    When mouse click and drag, it draws a stroke.

--- 08-HTML5Canvas/README.md
+++ 08-HTML5Canvas/README.md
@@ -6,6 +6,35 @@
 
 ## 👨‍🎓학습 목표
 
+- canvas 익히기
+
 ## ✍️개발 순서
 
+1. canvas 생성
+2. 2D context 설정
+3. 마우스를 누른채(mousedown)로 움직일 때(mousemove) 그림이 그려지도록 함
+
 ## 🎮JS 문법
+
+### canvas tag
+
+> [canvas tag MDN](https://developer.mozilla.org/ko/docs/Web/HTML/Canvas/Tutorial/Basic_usage)
+
+```
+<canvas> 엘리먼트는 고정 크기의 드로잉 영역을 생성하고 하나 이상의 렌더링 컨텍스(rendering contexts)를 노출하여, 출력할 컨텐츠를 생성하고 다루게 됩니다.
+```
+
+`getContext()` 메서드를 호출해서 2D나 3D 렌더링이 가능
+
+> [CanvasRenderingContext2D MDN](https://developer.mozilla.org/ko/docs/Web/API/CanvasRenderingContext2D)
+
+2D context의 여러 값들을 설정해줄 수 있다.
+
+### [lastX, lastY] = [e.offsetX, e.offsetY]
+
+```javascript
+lastX = e.offsetX;
+lastY = e.offsetY;
+```
+
+이 코드를 이렇게 `[lastX, lastY] = [e.offsetX, e.offsetY];` 한 줄로 표현 가능
---
+++ 08-HTML5Canvas/script.js
@@ -0,0 +1,33 @@
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+
+// canvas resize
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+  if (!isDrawing) return; // mouseDown이 아닐 때 그리지 않기
+
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  // go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  // draw
+  ctx.stroke();
+
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", () => (isDrawing = true));
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
