91867d08e2396163e763c0dfcf2c354f43838a1d
Siromom <swon962@gmail.com>
2020-03-18 01:17:05 +0900

    [ADD] project11 - video toggle
    - video를 play/pause 시킬 때, icon이 적절히 변경되도록 함.
    - 삼항연산자 활용
    - 함수 기능 세분화

--- 11-CustomVideoPlayer/script.js
+++ 11-CustomVideoPlayer/script.js
@@ -9,17 +9,27 @@ const ranges = document.querySelectorAll(".player__slider");
 
 /* Build out functions */
 function togglePlay() {
-  // method를 호출하는 다른 방법
+  /* method를 호출하는 다른 방법 */
   const method = video.paused ? "play" : "pause";
   video[method]();
 
+  /* 내가 구현한 코드 */
   //   if (video.paused) {
   //     video.play();
+  //     toggle.innerHTML = "❚❚";
   //   } else {
   //     video.pause();
+  //     toggle.innerHTML = "►";
   //   }
+}
+
+function updateButton() {
+  const icon = video.paused ? "►" : "❚❚";
+  toggle.textContent = icon;
 }
 
 /* Hook up the event listners */
 video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
 toggle.addEventListener("click", togglePlay);
