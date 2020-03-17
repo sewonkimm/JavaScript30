1689984310c1cc85146089c7f53b4c55fe7bde0d
Siromom <swon962@gmail.com>
2020-03-18 01:08:32 +0900

    [ADD] project11 - video toggle
    - video를 click 하거나 toggle button을 click하면 play, pause 될 수 있도록 함.
    
    - toggle 상태에 따라 toggle button 내용이 바뀌는 코드 추가 예정.

--- 11-CustomVideoPlayer/script.js
+++ 11-CustomVideoPlayer/script.js
@@ -1,1 +1,25 @@
+/* Get our elements */
+const player = document.querySelector(".player");
+const video = document.querySelector(".viewer");
+const progress = document.querySelector(".progress");
+const progressBar = document.querySelector(".progress__filled");
+const toggle = document.querySelector(".toggle");
+const skipButtons = document.querySelectorAll("[data-skip");
+const ranges = document.querySelectorAll(".player__slider");
 
+/* Build out functions */
+function togglePlay() {
+  // method를 호출하는 다른 방법
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+
+  //   if (video.paused) {
+  //     video.play();
+  //   } else {
+  //     video.pause();
+  //   }
+}
+
+/* Hook up the event listners */
+video.addEventListener("click", togglePlay);
+toggle.addEventListener("click", togglePlay);
