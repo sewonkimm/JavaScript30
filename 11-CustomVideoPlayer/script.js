c5489e335e13f7ec0265ceffe627c1465bbac716
Siromom <swon962@gmail.com>
2020-03-18 01:39:45 +0900

    [ADD] project11 - progress bar
    - progress bar를 클릭해 video time 변경하기 구현

--- 11-CustomVideoPlayer/script.js
+++ 11-CustomVideoPlayer/script.js
@@ -41,6 +41,11 @@ function handleProgress() {
   progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 /* Hook up the event listners */
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -53,3 +58,9 @@ skipButtons.forEach(button => button.addEventListener("click", skip));
 
 ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
 ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", e => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
